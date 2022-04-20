18cc50498c99def6e8d9b877d2a303ead877cbee
bijupv <pv.biju@gmail.com>
2022-04-20 19:25:33 +0530

    adding 3 ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("adding all model related to ML")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
